155270114c5f8cc758f12c245fe3dc610c557770
arrovain <arrovain@gmail.com>
2024-04-14 15:55:18 +0300

    finalize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-
+import PaginationBar from "@/components/PaginationBar";
 import ProductCard from "@/components/ProductCard";
 import { prisma } from "@/lib/db/prisma";
 import Image from "next/image";
